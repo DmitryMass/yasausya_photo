c02ba2286ef2702b2b1e066ddefda35edc7b6a66
MarS-27 <marchuk1992@gmail.com>
2023-08-23 13:52:16 +0300

    created nav-menu

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,62 +1,27 @@
-import clsx from "clsx";
-import { useRouter } from "next/router";
-import type { FC } from "react";
-import { LangButtonts } from "./LangButtonts";
+import { useState, type FC } from 'react';
+import { LangButtonts } from './LangButtonts';
+import { BurgerButton } from './BurgerButton';
+import { usePlaySound } from '@/src/hooks/usePlaySound';
+import { NavMenu } from './NavMenu';
 
 export const Header: FC = () => {
-  const { pathname } = useRouter();
+  const [isOpenNav, setToggleNav] = useState(false);
+  const { playSound } = usePlaySound();
+
+  const toggleNav = () => {
+    setToggleNav(!isOpenNav);
+    playSound();
+  };
 
   return (
-    <header
-      className={clsx(
-        pathname === "/" ? "h-screen" : null,
-        "px-5 pt-2.5 relative"
-      )}
-    >
-      <div className="flex items-center justify-end gap-8 mt-4 mr-4">
-        <div className="z-10 text-white">
+    <header className="px-9 pt-7 fixed w-full top-0 z-10">
+      <div className="flex items-center justify-end gap-8">
+        <div className="text-white z-20">
           <LangButtonts />
         </div>
-        <div
-          className={clsx(
-            "w-8 h-8 py-1 font-bold z-10 text-sand relative flex flex-col gap-1 items-center justify-between cursor-pointer hover:scale-x-75",
-            "after:w-full after:h-1 after:bg-sand hover:after:scale-x-100",
-            "before:w-full before:h-1 before:bg-sand hover:before:scale-x-50"
-          )}
-        >
-          <span className="w-full h-1 bg-sand" />
-        </div>
-        {/* <button
-          className="z-10 cursor-pointer ease-out duration-300"
-          // onClick={handleToggle}
-        >
-          <div
-            className={clsx(
-              "w-full flex flex-col gap-1 transition-all duration-700 ease-[cubic-bezier(0.68,-0.35,0.265,1.35)]"
-              // isToggled ? 'rotate-180' : null,
-            )}
-          >
-            <div
-              className={clsx(
-                "w-8 h-1 bg-sand transition-all duration-300 delay-200"
-                // isToggled ? 'rotate-45 translate-y-[10px]' : null,
-              )}
-            ></div>
-            <div
-              className={clsx(
-                "w-8 h-1 bg-sand transition-all duration-100 delay-300"
-                // isToggled ? 'bg-transparent' : 'bg-white',
-              )}
-            ></div>
-            <div
-              className={clsx(
-                "w-8 h-1 bg-sand transition-all duration-300 delay-200"
-                // isToggled ? '-rotate-45 -translate-y-[10px]' : null,
-              )}
-            ></div>
-          </div>
-        </button> */}
+        <BurgerButton isOpenNav={isOpenNav} toggleBurger={toggleNav} />
       </div>
+      <NavMenu isOpenNav={isOpenNav} closeNav={toggleNav} />
     </header>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,14 +14,14 @@ const Home: FC = () => {
 
   return (
     <>
-      <div className='bg-[url("/pictures/IMG_1033.JPG")] bg-no-repeat bg-cover bg-center w-full h-full absolute top-0 left-0 z-0'>
+      <div className='h-screen bg-[url("/pictures/IMG_1033.JPG")] bg-no-repeat bg-cover bg-center w-full'>
         <MainInfo />
-        <div className='left-0 absolute bottom-5 w-full mx-auto'>
+        <div className="left-0 absolute bottom-5 w-full mx-auto">
           <span
             onClick={handleClick}
-            className='text-white cursor-pointer text-center w-10 h-10 animate-pulse hover:animate-none rounded-full mx-auto flex items-center justify-center'
+            className="text-white cursor-pointer text-center w-10 h-10 animate-pulse hover:animate-none rounded-full mx-auto flex items-center justify-center"
           >
-            <img className='w-7 h-7' src='/icons/arrow-icon.svg' alt='logo' />
+            <img className="w-7 h-7" src="/icons/arrow-icon.svg" alt="logo" />
           </span>
         </div>
       </div>
