5010e5d0793c9ea2e6ee3764a97d162d8943bf45
DmitryMass <dimamikron41@gmail.com>
2023-08-24 14:42:18 +0300

    fixed back to top btn

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -3,6 +3,7 @@ import { Header } from './Header';
 import { Footer } from './Footer';
 import { Raleway } from 'next/font/google';
 import clsx from 'clsx';
+import { ScrollTop } from '../ui/ScrollTop';
 
 const raleway = Raleway({ subsets: ['latin'] });
 
@@ -13,8 +14,9 @@ type MainLayoutProps = {
 export const MainLayout: FC<MainLayoutProps> = ({ children }) => {
   return (
     <div className={clsx(raleway.className, 'flex flex-col min-h-full')}>
+      <ScrollTop />
       <Header />
-      <main className='flex-1'>{children}</main>
+      <main className="flex-1">{children}</main>
       <Footer />
     </div>
   );
